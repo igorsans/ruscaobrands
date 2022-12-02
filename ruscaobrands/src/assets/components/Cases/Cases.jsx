a98f305a2134312e7c337a74c3885b2062eef36b
igorsans <igor.santos0@hotmail.com>
2022-12-02 17:48:30 -0300

    identação

--- ruscaobrands/src/assets/components/Cases/Cases.jsx
+++ ruscaobrands/src/assets/components/Cases/Cases.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import CasesStyle from './Style.js'
-import { BsArrowRight } from "react-icons/bs"
+import React from "react";
+import CasesStyle from "./Style.js";
+import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
-const Cases = ({ setCases }) => {
-  
+const Cases = ({ handleCaseChange }) => {
   const caseteste = {
-    titulo: 'testando',
-    descricao: 'lorem ipsum etc aklsdjasd lkajsdakls asjkdaklsdjk askldjaskld',
-    img: 'test'
-  }
+    titulo: "testando",
+    descricao: "lorem ipsum etc aklsdjasd lkajsdakls asjkdaklsdjk askldjaskld",
+    img: "test",
+  };
   return (
     <CasesStyle>
-        <h2>CASES</h2>
-        <hr />
-        <div className='cases'>
-            <img onClick={() => setCases(caseteste)} className='casesImg' src="/Biggies.jpg" alt="" />
-            <img className='casesImg' src="/Biggies.jpg" alt="" />
-            <img className='casesImg' src="/Biggies.jpg" alt="" />
-            <img className='casesImg' src="/Biggies.jpg" alt="" />
-            <img className='casesImg' src="/Biggies.jpg" alt="" />
-            <img className='casesImg' src="/Biggies.jpg" alt="" />
-        </div>
-        <button className='seeAll'>See All <BsArrowRight/></button>
+      <h2>CASES</h2>
+      <hr />
+      <div className="cases">
+        <Link to={"/cases"}>
+          <img
+            onClick={() => handleCaseChange(caseteste)}
+            className="casesImg"
+            src="/Biggies.jpg"
+            alt=""
+          />
+          <img className="casesImg" src="/Biggies.jpg" alt="" />
+          <img className="casesImg" src="/Biggies.jpg" alt="" />
+          <img className="casesImg" src="/Biggies.jpg" alt="" />
+          <img className="casesImg" src="/Biggies.jpg" alt="" />
+          <img className="casesImg" src="/Biggies.jpg" alt="" />
+        </Link>
+      </div>
+      <button className="seeAll">
+        See All <BsArrowRight />
+      </button>
     </CasesStyle>
-  )
-}
+  );
+};
 
-export default Cases
+export default Cases;
