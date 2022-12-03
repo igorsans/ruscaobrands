cf2b8cd65fb67efd4d60be5d5358fe838926a0ab
igorsans <igor.santos0@hotmail.com>
2022-12-03 18:44:41 -0300

    Use Navigate

--- ruscaobrands/src/assets/components/AboutMe/AboutMe.jsx
+++ ruscaobrands/src/assets/components/AboutMe/AboutMe.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import me from '/me.png'
 import AboutMeStyle from './Style.js'
 import Button from '../Button/Button.jsx'
+import { useNavigate } from 'react-router-dom';
 
 const AboutMe = () => {
+  const navigate = useNavigate();
   return (
     <AboutMeStyle>
         <div className='textAboutMe'>
             <h3>BRAND DESIGNER SPECIALIST.</h3>
             <h2>I help your brand stand out from your competitors by building an authentic connection with your audience.</h2>
-            <Button text="MORE ABOUT ME"/>
+            <Button nav={() => navigate('/aboutme')} text="MORE ABOUT ME"/>
         </div>
         <div>
             <img src={me} alt="" />
