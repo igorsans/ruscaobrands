456524442b656337f00d1c77d188e90725ae151e
igorsans <igor.santos0@hotmail.com>
2022-12-06 18:46:13 -0300

    refactor: ajustando thema do footer por pagina

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Intro from '../../assets/components/IntroAboutMe/Intro.jsx'
 import Contact from '../../assets/components/MoreAboutContact/Contact.jsx'
 import More from '../../assets/components/MoreAboutMe/More.jsx'
 import StyledAbout from './Style.js'
 
 const About = ({setInHome}) => {
+  useEffect(() => {
+    setInHome(false)
+  }, [])
   return (
     <StyledAbout>
         <Intro/>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import AboutMe from "../../assets/components/AboutMe/AboutMe";
 import Cases from "../../assets/components/Cases/Cases";
 import Contact from "../../assets/components/Contact/Contact";
@@ -6,10 +7,14 @@ import Intro from "../../assets/components/Intro/Intro";
 import WorkTogether from "../../assets/components/WorkTogether/WorkTogether";
 
 const Home = ({handleCaseChange, setInHome}) => {
+  useEffect(() => {
+    setInHome(true)
+  }, [])
+  
   return (
     <>
       <Intro />
-      <Cases handleCaseChange={handleCaseChange} />
+      <Cases setInHome={setInHome} handleCaseChange={handleCaseChange} />
       <AboutMe setInHome={setInHome} />
       {/* <Carousel/> */}
       <WorkTogether />
